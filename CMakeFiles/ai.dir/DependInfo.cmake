cf4334cf31d32350a5e6d39d46e0fa96d13fc28e
Gladkikh Prohor <prohor33@gmail.com>
2015-11-14 23:00:03 +0300

    initial

---
+++ CMakeFiles/ai.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/prohor/Code/rus-ai-cup-2015/MyStrategy.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/MyStrategy.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/RemoteProcessClient.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/RemoteProcessClient.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/Runner.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/Runner.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/Strategy.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/Strategy.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/csimplesocket/ActiveSocket.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/csimplesocket/ActiveSocket.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/csimplesocket/HTTPActiveSocket.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/csimplesocket/HTTPActiveSocket.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/csimplesocket/PassiveSocket.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/csimplesocket/PassiveSocket.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/csimplesocket/SimpleSocket.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/csimplesocket/SimpleSocket.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Bonus.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Bonus.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Car.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Car.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/CircularUnit.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/CircularUnit.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Game.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Game.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Move.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Move.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/OilSlick.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/OilSlick.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Player.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Player.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/PlayerContext.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/PlayerContext.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Projectile.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Projectile.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/RectangularUnit.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/RectangularUnit.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/Unit.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/Unit.cpp.o"
+  "/Users/prohor/Code/rus-ai-cup-2015/model/World.cpp" "/Users/prohor/Code/rus-ai-cup-2015/CMakeFiles/ai.dir/model/World.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
